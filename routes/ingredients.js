32da6eac95a462cf4da2efbef16bf90781355971
Ben Carpenter <bcaprenter@turissystems.com>
2014-07-08 16:24:29 -0500

    Updated display with hogan

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -2,19 +2,28 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
-/* List all ingredients */
 router.get('/', function(req, res) {
-  mongoose.model('ingredients').find(req.query,function(err,ingredients){
-  	res.render('ingredients', { title: 'MixUp - Ingredients', ingredients: ingredients });
-  	//res.send(ingredients);
-  });
+  var searchName=req.query.name;
+  //search by name
+  if (searchName!==undefined){
+  	var regex = new RegExp(searchName,"i");
+  	mongoose.model('ingredients').find({name: regex},function(err,ingredients){
+  	res.render('ingredients', { title: 'MixUp - Ingredients', ingredients: ingredients,query: req.query });
+  	});
+  }	
+
+  //show all
+  else{
+    mongoose.model('ingredients').find(function(err,ingredients){
+    res.render('ingredients', { title: 'MixUp - Ingredients', ingredients: ingredients });
+    });
+  }
 });
 
 /*find an ingredient by ID */
 router.get('/:ID',function(req,res){
   mongoose.model('ingredients').find({_id: req.params.ID}, function(err,ingredients){
   	res.render('ingredient', { title: "MixUp - " + ingredients[0].name, ingredient: ingredients[0] });
-  	//res.send(ingredients);
   });
 });
 
