6cfeaf64b7d3519c696d3deef62cd3b1462cd197
Ben Carpenter <bcaprenter@turissystems.com>
2014-07-08 11:21:24 -0500

    Basic Hogan templating for the ingredient pages

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -5,14 +5,16 @@ var mongoose = require('mongoose');
 /* List all ingredients */
 router.get('/', function(req, res) {
   mongoose.model('ingredients').find(req.query,function(err,ingredients){
-  	res.send(ingredients);
+  	res.render('ingredients', { title: 'MixUp - Ingredients', ingredients: ingredients });
+  	//res.send(ingredients);
   });
 });
 
 /*find an ingredient by ID */
 router.get('/:ID',function(req,res){
   mongoose.model('ingredients').find({_id: req.params.ID}, function(err,ingredients){
-  	res.send(ingredients);
+  	res.render('ingredient', { title: "MixUp - " + ingredients[0].name, ingredient: ingredients[0] });
+  	//res.send(ingredients);
   });
 });
 
